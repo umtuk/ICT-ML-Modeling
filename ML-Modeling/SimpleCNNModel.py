a8f40a5a36507d18d5d3a592b82f8c7de82dc120
umtuk <e9001014@gmail.com>
2019-07-19 19:12:30 +0900

    Create SimpleCNNModel

--- ML-Modeling/SimpleCNNModel.py
+++ ML-Modeling/SimpleCNNModel.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras import layers
 
 tf.keras.backend.clear_session()
@@ -18,19 +19,15 @@ WSIZE = 32 # input 이미지 width 크기
 # 기존 모델을 불러오고 싶다면 다음 코드 추가
 # model = tf.keras.models.load_model('./saved/'+FileNameV+'.h5')
 
-inputs = tf.keras.Input(shape=(WSIZE, HSIZE, 3), name='img')
-
-x = layers.Conv2D(32, 3, activation='relu')(inputs)
-x = layers.Conv2D(32, 3, activation='relu')(x)
-block_1_output = layers.MaxPooling2D(2)(x)
-
+encoder_input = keras.Input(shape=(WSIZE, HSIZE, 3), name='img')
+x = layers.Conv2D(32, kernel_size=3, padding='same', activation='relu')(encoder_input)
+x = layers.Conv2D(32, kernel_size=3, padding='same', activation='relu')(x)
+x = layers.MaxPooling2D(2)(x)
 x = layers.Flatten()(x)
-block_2_output = layers.add([x, block_1_output])
+encoder_output = layers.Dense(10, activation='softmax')(x)
 
-outputs = layers.Dense(10, activation='softmax')(x)
-
-model = tf.keras.Model(inputs, outputs, name=FILENAME)
-model.summary()
+encoder = keras.Model(encoder_input, encoder_output, name='encoder')
+encoder.summary()
 
 # 학습 후 저장하고 싶다면 다음 코드 추가
 # model.save_weights('./saved/'+FILENAMEV+'.h5', save_format='h5')
